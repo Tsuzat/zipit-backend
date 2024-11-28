2f4998a91ae005837e50a208ca3d392c53433d20
Tsuzat <dev.tsuzat@gmail.com>
2024-11-28 22:14:53 +0530

    add: Poolsize config for database connection pooling

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"errors"
+	"time"
 
 	"github.com/Tsuzat/zipit-go-fiber/config"
 	"github.com/go-pg/pg/v10"
@@ -12,6 +13,11 @@ func ConnectDB() error {
 		return errors.New("DB_URL is not set")
 	}
 	opts, err := pg.ParseURL(config.DB_URL)
+	// Set connection pooling options
+	opts.PoolSize = 100                 // Set maximum number of connections in the pool
+	opts.MinIdleConns = 10              // Maintain a minimum number of idle connections
+	opts.MaxConnAge = 5 * time.Minute   // Close connections older than this duration
+	opts.IdleTimeout = 15 * time.Second // Timeout for idle connections
 	if err != nil {
 		return err
 	}
